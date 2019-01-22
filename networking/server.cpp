47a93993d33c1dc779b60732b8192ba9a5707aff
56KbModem <nick-snel@hotmail.com>
2019-01-22 10:38:40 +0100

    added release for server, this supresses debug messages

--- networking/server.cpp
+++ networking/server.cpp
@@ -1,9 +1,4 @@
-//
-// Created by ramon on 19-1-2019.
-//
-
 #include "server.hpp"
-
 
 
 server::server(unsigned short serverPort)
@@ -19,16 +14,22 @@ sf::Socket::Status server::serverReceive()
 {
 
     if (socket.receive(rawPacket, lastIp, lastPort) != sf::Socket::Done) {
+#if DEBUG
         NETWORK_INFO("received nothing");
+#endif // DEBUG
         return sf::Socket::NotReady;
     }
+#if DEBUG
     NETWORK_INFO("Received data");
+#endif // DEBUG
     clientIps.insert(lastIp);
     return sf::Socket::Done; 
 }
 
 sf::Socket::Status server::playerSend() {
+#if DEBUG
     NETWORK_INFO("Server send function");
+#endif // DEBUG
     std::for_each(clientIps.begin(), clientIps.end(), [&](sf::IpAddress client){
         if (client != lastIp) {
             if (socket.send(rawPacket, client, lastPort) != sf::Socket::Done) {
@@ -41,8 +42,9 @@ sf::Socket::Status server::playerSend() {
 }
 
 sf::Socket::Status server::timeSend(){
-
+#if DEBUG
     NETWORK_INFO("Server send function");
+#endif // DEBUG
     std::for_each(clientIps.begin(), clientIps.end(), [&](sf::IpAddress client){
        if(socket.send(rawPacket, client, lastPort) != sf::Socket::Done){
             NETWORK_ERROR("Sending failed");
