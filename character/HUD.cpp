a2d5ccb00ea0a7ab8845fc9ed5f973bd146e812b
MarcDirven <marc.dirven@student.hu.nl>
2019-01-22 10:15:42 +0100

    edited character/main/topforce included thread

--- character/HUD.cpp
+++ character/HUD.cpp
@@ -8,9 +8,9 @@ namespace tf {
 HUD::HUD(sf::RenderWindow &window, sf::View& view) :
     MoveableScreenObject(window),
     currentAmmo(30),
+    clipSize(30),
     totalAmmo(90),
     currentHealth(100),
-    clipSize(30),
     view(view)
     {
         if (!font.loadFromFile(FONT)) {
@@ -19,17 +19,16 @@ HUD::HUD(sf::RenderWindow &window, sf::View& view) :
         configText();
     }
 
-
 void HUD::updateText() {
     ammoText.setString("Ammo: " + std::to_string(currentAmmo) + '/' + std::to_string(totalAmmo));
     healthText.setString("HP: " + std::to_string(currentHealth) + "/100");
 }
 
 void HUD::checkAmmo() {
-    if (currentAmmo <= 10) {
+    if (currentAmmo < 10) {
         ammoText.setFillColor(sf::Color::Red);
     }
-    if (currentHealth <= 30) {
+    if (currentHealth < 30) {
         healthText.setFillColor(sf::Color::Red);
     }
 }
@@ -40,11 +39,11 @@ void HUD::configText() {
     healthText.setCharacterSize(24);
     reloadText.setCharacterSize(24);
     reloadText.setFillColor(sf::Color::Red);
-    // set font
+
     ammoText.setFont(font);
     healthText.setFont(font);
     reloadText.setFont(font);
-    // this text does not change over time, that's why it is initialized already
+
     reloadText.setString("Press R to reload");
 }
 
@@ -61,7 +60,7 @@ void HUD::update() {
     checkAmmo();
     updateText();
 
-    // position
+    // update position relative to view
     sf::Vector2f viewCenter = view.getCenter();
     healthText.setPosition(viewCenter.x + 750, viewCenter.y + 350);
     ammoText.setPosition(viewCenter.x + 750, viewCenter.y + 400);
@@ -73,24 +72,10 @@ void HUD::decreaseHealth(const int &amount) {
 }
 
 void HUD::decreaseAmmo(const int &amount) {
-    if (currentAmmo >= 0) {
-        currentAmmo -= amount;
-    }
+    currentAmmo -= amount;
 }
 
 void HUD::reload() {
-    /*return if all ammo equals 0 or if current magazine is already full*/
-    if (currentAmmo != clipSize) {
-        if (totalAmmo != 0) {
-            int needed = clipSize - currentAmmo;
-            if (needed < totalAmmo) {
-                totalAmmo -= needed;
-                currentAmmo += needed;
-            } else {
-                currentAmmo += totalAmmo;
-                totalAmmo = 0;
-            }
-        }
-    }
+
 }
 } // namespace tf
